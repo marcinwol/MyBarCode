5eccbdff2a7d8b2503a775e63ce107adf59fb04b
marcin <wolskint@o2.pl>
2015-06-05 15:08:56 +0800

    Basic threads added for testing.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,50 @@
 #include <iostream>
+#include <thread>
+#include <mutex>
+#include <atomic>
 
 #include "src/MwBarCode.h"
 
 using namespace Magick;
+
+
+namespace {
+
+
+   size_t i {0};
+
+    MwBarCode::paths_vector found_files ;
+    vector<MwColor> avg_pixels;
+
+    mutex g_mutex;
+
+
+    void process_files()
+    {
+
+        while (i < found_files.size() )
+        {
+            path a_file;
+
+            {
+                std::lock_guard<mutex> lock3(g_mutex);
+                a_file = found_files.at(++i);
+                cout << i << endl;
+            }
+
+            // fmt::print("{:d}/{:d}: {:s}\n", i, found_files.size(), a_file.string());
+
+            MwImage mwi {a_file};
+
+            {
+                std::lock_guard<mutex> lock2(g_mutex);
+                avg_pixels.emplace_back(mwi.getAvgPixel());
+            }
+        }
+
+    }
+
+}
 
 int main(int ac, const char* av[]) {
 
@@ -23,35 +65,43 @@ int main(int ac, const char* av[]) {
     }
 
     // get all image paths in an input folder
-    MwBarCode::paths_vector found_files ;
     found_files = app.getPaths();
 
-    // vector to store avarage color of each image
-    vector<MwColor> avg_pixels;
+//
+//    size_t no_of_imgs = found_files.size();
+//
+//    for (const path& a_file: found_files)
+//    {
+//
+//        fmt::print("{:d}/{:d}: {:s}\n", i, no_of_imgs, a_file.string());
+//
+//        MwImage mwi {a_file};
+//
+//        avg_pixels.emplace_back(mwi.getAvgPixel());
+//
+//        ++i;
+//
+//    }
 
-    size_t i {1};
-    size_t no_of_imgs = found_files.size();
+    vector<thread> thrds;
 
-    for (const path& a_file: found_files)
+    for (size_t ti = 0; ti <= 10; ++ti)
     {
-
-        fmt::print("{:d}/{:d}: {:s}\n", i, no_of_imgs, a_file.string());
-
-        MwImage mwi {a_file};
-
-        avg_pixels.emplace_back(mwi.getAvgPixel());
-
-        ++i;
-
+        thrds.push_back(thread(process_files));
     }
 
+
+    for (thread& t: thrds)
+    {
+        t.join();
+    }
 
 
     Magick::Image bar_code;
 
     bar_code = app.makeBarCode(avg_pixels);
 
-    bar_code.write("/home/m/Desktop/out.png");
+    bar_code.write("/home/marcin/Desktop/out.png");
 
    // app.test();
 
